6038f049fd77a84be181ba7a9e10e637f01a33c5
maciejraczuk maciejraczuk <mr429173@students.mimuw.edu.pl>
2025-02-02 02:22:31 +0100

    wykres celnosci

--- plot_accuracies.py
+++ plot_accuracies.py
@@ -3,12 +3,15 @@ from constants import INITIAL_CUTOFF, TOP
 
 
 def plot_accuracies_(accuracies, baseline):
-    plt.hist([[acc for acc, _ in accuracies_] for accuracies_ in accuracies.values()], label=[*accuracies.keys()])
-    plt.axvline(baseline, color='black', linestyle='-', linewidth=1, label='baseline')
     colors = ['red', 'green', 'blue']
-    thresholds = [sorted(accuracies_, key=lambda x: x[0])[-int(TOP*len(accuracies_))] for accuracies_ in accuracies.values()]
+    plt.hist([[acc for acc, _ in accuracies_] for accuracies_ in accuracies.values()], label=[*accuracies.keys()],
+             color=colors)
+    plt.axvline(baseline, color='black', linestyle='-', linewidth=1, label='baseline')
+    thresholds = [sorted(accuracies_, key=lambda x: x[0])[-int(TOP*len(accuracies_))][0] for accuracies_ in accuracies.values()]
     labels = accuracies.keys()
     for color, threshold, label in zip(colors, thresholds, labels):
-        plt.axvline(threshold, color=color, linestyle='--', linewidth=1, label=label)
+        plt.axvline(threshold, color=color, linestyle='--', linewidth=1)
     plt.legend(loc='upper right')
+    plt.xlabel('accuracy')
+    plt.ylabel('count')
     plt.show()
