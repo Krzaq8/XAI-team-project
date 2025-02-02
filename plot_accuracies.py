20ca7578571f3b1d15a4d1ca75ee8fa5f9b8e75d
maciejraczuk maciejraczuk <mr429173@students.mimuw.edu.pl>
2025-02-02 01:49:53 +0100

    parametr dodany

--- plot_accuracies.py
+++ plot_accuracies.py
@@ -4,5 +4,6 @@ from constants import INITIAL_CUTOFF, TOP
 
 def plot_accuracies_(accuracies):
     plt.hist([[acc for acc, _ in accuracies_] for accuracies_ in accuracies.values()], label=[*accuracies.keys()])
+    plt.axvline(BASELINE, color='green', linestyle='-', linewidth=1, label='baseline')
     plt.legend(loc='upper right')
     plt.show()
