779dae982735bd91bf980a5141dfc70bd2fe8afc
maciejraczuk maciejraczuk <mr429173@students.mimuw.edu.pl>
2025-02-02 01:28:29 +0100

    plot accuracies

--- plot_accuracies.py
+++ plot_accuracies.py
@@ -1,17 +1,3 @@
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=(SEED+1))
-rashomon_sets = {}
-rashomon_sets_acc_lower_bounds = {}
-accuracies = {}
-for model_class in MODELS:
-    rashomon_sets[model_class.__name__] = []
-    accuracies[model_class.__name__] = []
-    for kwargs in rashomon_sets_params[model_class.__name__]:
-        if model_class.__name__ == 'SVMClassifier':
-            model = model_class(probability=True, **kwargs)
-        else:
-            model = model_class(**kwargs)
-        model.fit(X_train, y_train)
-        acc = np.mean(model.predict(X_test) == np.array(y_test))
-        accuracies[model_class.__name__].append(acc)
-        rashomon_sets[model_class.__name__].append(model)
-    rashomon_sets_acc_lower_bounds[model_class.__name__] = min(accuracies[model_class.__name__])
+from constants import INITIAL_CUTOFF, TOP
+
+def plot_accuracies_(accuracies):
