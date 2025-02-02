303cb0bb4ca6f404f0ad900368ce26b47a670bbd
maciejraczuk maciejraczuk <mr429173@students.mimuw.edu.pl>
2025-02-02 01:43:30 +0100

    ...

--- plot_accuracies.py
+++ plot_accuracies.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
 from constants import INITIAL_CUTOFF, TOP
 
+
 def plot_accuracies_(accuracies):
+    plt.hist([[acc for acc, _ in accuracies_] for accuracies_ in accuracies.values()], label=[*accuracies.keys()])
+    plt.legend(loc='upper right')
+    plt.show()
